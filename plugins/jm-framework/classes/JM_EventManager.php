ae3aa7c5f0a831d79c9603b45654521a555874f5
Jon McDonald <jon@onewebcentric.com>
2012-11-13 11:42:39 -0700

    Mailing List / Footer
    * Adds reject/cancel synchronization for mailing list
    * Adds proper title synchronization for mailing lists and events
    * Mailing Lists now drop upon event being dropped
    
    * Push footer up

--- plugins/jm-framework/classes/JM_EventManager.php
+++ plugins/jm-framework/classes/JM_EventManager.php
@@ -23,7 +23,13 @@ class JM_EventManager
 		add_action( 'wp_insert_post', array( $this, 'new_post_list' ) );
 	
 		// When a user creates a booking, add them to the mailing list
-		add_action( 'em_bookings_added', array( $this, 'add_user_to_list' ) );
+		add_action( 'em_booking', array( $this, 'add_user_to_list' ) );
+
+		// When a user or admin cancels a booking, remove them
+		add_action( 'em_booking', array( $this, 'remove_user_from_list' ) );
+
+		// Remove a event upon deletion of post
+		add_action( 'post_deleted', array( $this, 'remove_list' ) );
 	}
 
 	/**
@@ -37,7 +43,22 @@ class JM_EventManager
 	{
 		// Get the post object, and format a name
 		$post = get_post( $post_id );
-		$name = 'Event: ' . $post->post_title . '-' . $post->ID;
+		$name = 'Event: ' . $post->post_title;
+
+		// Get the term
+		$term = get_term_by( 'slug', $post->ID, 'MailPress_mailing_list' );
+
+		if( $term )
+		{
+			wp_update_term($term->term_id, 
+				'MailPress_mailing_list', 
+				array(
+    				'name' => $post->post_title
+    				)
+				);
+
+			return;
+		}
 
 		// If valid, add this as a new mailing list
 		if( $post->post_type == 'event' && 
@@ -64,6 +85,8 @@ class JM_EventManager
 	 */
 	public function add_user_to_list( $EM_Booking )
 	{
+		if( $EM_Booking->get_status() != 'Approved' )
+			return;
 		// Get the user ID, and MailPress ID
 		$user       = $EM_Booking->get_person();
 		$user_id    = $user->ID;
@@ -94,6 +117,56 @@ class JM_EventManager
 		wp_set_object_terms( $user_mp_id, 
 			$all_terms, 
 			'MailPress_mailing_list' );
+	}
+
+	/**
+	 * 
+	 */
+	public function remove_user_from_list( $EM_Booking )
+	{
+		if( $EM_Booking->get_status() != 'Cancelled' && $EM_Booking->get_status() != 'Rejected' && $EM_Booking->get_status() != false )
+			return;
+
+		// Get the user ID, and MailPress ID
+		$user       = $EM_Booking->get_person();
+		$user_id    = $user->ID;
+		$user_mp_id = get_user_meta( $user_id, '_MailPress_sync_wordpress_user', true );
+	
+		// Get the event
+		$event      = $EM_Booking->get_event();
+		$event_name = $event->event_name;
+		$event_id   = $event->ID;
+
+		// Get the term to remove
+		$term       = get_term_by( 'slug', $event_id, 'MailPress_mailing_list' );
+		$term_id    = $term->term_id;
+		$term_slug  = $term->slug;
+
+		$existing   = wp_get_object_terms( $user_mp_id, 'MailPress_mailing_list' );
+		$all_terms  = array();     
+
+		// Loop through and add every term EXCEPT the event we're cancelling from
+		foreach( $existing as $current )
+		{
+			if( $current->slug == $event_id )
+				continue;
+			$all_terms[] = $current->slug;
+		}
+
+		// Add the user to the list
+		wp_set_object_terms( $user_mp_id, 
+			$all_terms, 
+			'MailPress_mailing_list' );
+	}
+
+	public function remove_list( $post_id )
+	{
+		$term = get_term_by( 'slug', $post_id, 'MailPress_mailing_list' );
+
+		if( $term )
+		{
+			wp_delete_term( $term->term_id, 'MailPress_mailing_list');
+		}
 	}
 }
 
